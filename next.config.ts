bc34ac4a3ae3e1183208197fca62ac5a1f9e01a4
jilicuerda <132850859+jilicuerda@users.noreply.github.com>
2026-02-15 09:26:30 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // This forces Vercel to deploy even if there are Type/Lint errors
   typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    ignoreBuildErrors: true, // Keep this one, it is still valid
   },
 };
 
