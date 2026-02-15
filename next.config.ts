ae79a5e0e638afa0fce621c19a5b81e7aa87f0c1
jilicuerda <132850859+jilicuerda@users.noreply.github.com>
2026-02-15 09:32:35 +0100

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // Keep this one, it is still valid
-  },
-};
-
-export default nextConfig;
